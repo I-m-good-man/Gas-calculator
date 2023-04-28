feb16187bbb89a21275ed0fcb23c24a84304bb97
Marat <maratxat@ya.ru>
2023-04-28 16:05:37 +0300

    Work with conflicts of checkboxes is done

--- ui/coordwidget.py
+++ ui/coordwidget.py
@@ -12,10 +12,23 @@ class CoordWidget(QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.id_widget = id_widget
-        # self.ui.groupBox.setTitle(str(id_widget))
+
         self.ui.delete_button.clicked.connect(self.press_del)
+
+        # делаем подробный выбор подчекбоксов объема и массы недоступными, пока не выберут чекбокс объема или массы
+        self.ui.g_checkbox.setCheckable(False)
+        self.ui.percent_checkbox.setCheckable(False)
+        self.ui.ml_checkbox.setCheckable(False)
+        self.ui.percent_checkbox_2.setCheckable(False)
+
+        # обрабатываем конфликты между чекбоксами
         self.ui.volume_checkbox.clicked.connect(self.press_volume_checkbox)
+        self.ui.ml_checkbox.clicked.connect(self.press_ml_checkbox)
+        self.ui.percent_checkbox_2.clicked.connect(self.press_percent_checkbox_2)
+
         self.ui.mass_checkbox.clicked.connect(self.press_mass_checkbox)
+        self.ui.g_checkbox.clicked.connect(self.press_g_checkbox)
+        self.ui.percent_checkbox.clicked.connect(self.press_percent_checkbox)
 
     def press_del(self):
         self.ui.groupBox.setParent(None)
@@ -23,20 +36,68 @@ class CoordWidget(QWidget):
 
     def press_volume_checkbox(self):
         """
-        Если нажали на выбор объема, до сбрасываем все чекбоксы массы
+        Если нажали на выбор объема, то обрабатываем чекбоксы
         :return:
         """
+        # чекбокс массы - false
         self.ui.mass_checkbox.setChecked(False)
+
+        # чекбоксы за подробный выбор массы делаем false и некликабельными
         self.ui.g_checkbox.setChecked(False)
+        self.ui.g_checkbox.setCheckable(False)
         self.ui.percent_checkbox.setChecked(False)
+        self.ui.percent_checkbox.setCheckable(False)
+
+        # делаем кликабельными чекбоксы объема
+        self.ui.ml_checkbox.setCheckable(True)
+        self.ui.percent_checkbox_2.setCheckable(True)
+
+    def press_g_checkbox(self):
+        """
+        Если чекбокс прцоентов был уже выбран, то его обнуляем
+        :return:
+        """
+        if self.ui.percent_checkbox.checkState():
+            self.ui.percent_checkbox.setChecked(False)
+
+    def press_percent_checkbox(self):
+        """
+        Если чекбокс граммов был уже выбран, то его обнуляем
+        :return:
+        """
+        if self.ui.g_checkbox.checkState():
+            self.ui.g_checkbox.setChecked(False)
 
     def press_mass_checkbox(self):
         """
-        Если нажали на выбор объема, до сбрасываем все чекбоксы объема
+        Если нажали на выбор массы, то обрабатываем чекбоксы
         :return:
         """
+        # чекбокс объема - false
         self.ui.volume_checkbox.setChecked(False)
+
+        # чекбоксы за подробный выбор объема делаем false и некликабельными
         self.ui.ml_checkbox.setChecked(False)
+        self.ui.ml_checkbox.setCheckable(False)
         self.ui.percent_checkbox_2.setChecked(False)
+        self.ui.percent_checkbox_2.setCheckable(False)
 
+        # делаем кликабельными чекбоксы массы
+        self.ui.g_checkbox.setCheckable(True)
+        self.ui.percent_checkbox.setCheckable(True)
 
+    def press_ml_checkbox(self):
+        """
+        Если чекбокс процентов был уже выбран, то его обнуляем
+        :return:
+        """
+        if self.ui.percent_checkbox_2.checkState():
+            self.ui.percent_checkbox_2.setChecked(False)
+
+    def press_percent_checkbox_2(self):
+        """
+        Если чекбокс мл был уже выбран, то его обнуляем
+        :return:
+        """
+        if self.ui.ml_checkbox.checkState():
+            self.ui.ml_checkbox.setChecked(False)
