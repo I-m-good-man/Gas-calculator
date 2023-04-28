1b7124b7f57152609ce122b4da552de745bf00a0
Marat <maratxat@ya.ru>
2023-04-28 15:48:42 +0300

    Implement only one choice between volume checkbox and mass checkbox

--- ui/coordwidget.py
+++ ui/coordwidget.py
@@ -14,7 +14,29 @@ class CoordWidget(QWidget):
         self.id_widget = id_widget
         # self.ui.groupBox.setTitle(str(id_widget))
         self.ui.delete_button.clicked.connect(self.press_del)
+        self.ui.volume_checkbox.clicked.connect(self.press_volume_checkbox)
+        self.ui.mass_checkbox.clicked.connect(self.press_mass_checkbox)
 
     def press_del(self):
         self.ui.groupBox.setParent(None)
         self.delete.emit(self.id_widget)
+
+    def press_volume_checkbox(self):
+        """
+        Если нажали на выбор объема, до сбрасываем все чекбоксы массы
+        :return:
+        """
+        self.ui.mass_checkbox.setChecked(False)
+        self.ui.g_checkbox.setChecked(False)
+        self.ui.percent_checkbox.setChecked(False)
+
+    def press_mass_checkbox(self):
+        """
+        Если нажали на выбор объема, до сбрасываем все чекбоксы объема
+        :return:
+        """
+        self.ui.volume_checkbox.setChecked(False)
+        self.ui.ml_checkbox.setChecked(False)
+        self.ui.percent_checkbox_2.setChecked(False)
+
+
